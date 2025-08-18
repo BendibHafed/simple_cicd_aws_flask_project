c789c8ef41b31609b8aa3e4169e3fe843e0eedee
BendibHafed <hafededdinebendib@gmail.com>
2025-08-18 16:31:10 +0200

    CI workflow - Terraform main edit

--- app/app.py
+++ app/app.py
@@ -1,19 +1,14 @@
 from flask import Flask
-from awslambdaric.lambda_context import LambdaContext
-from aws_lambda_wsgi import response as wsgi_response
+import awsgi  # provided by aws-wsgi
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
     return "Hello CI/CD"
 
-
-def lambda_handler(event, context: LambdaContext):
-    from aws_lambda_wsgi import handle_request
-    return handle_request(app, event, context)
-
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
