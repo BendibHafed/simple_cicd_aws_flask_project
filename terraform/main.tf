190756e4b13237e211007d55e404d61367d81e9b
BendibHafed <hafededdinebendib@gmail.com>
2025-08-18 16:23:51 +0200

    CI workflow - Terraform main edit

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+    region = "eu-west-3"
+}
+
+resource "aws_iam_role" "lambda_role" {
+    name = "flask_lambda_role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+                Service = "lambda.amazonaws.com"
+            }
+        }]
+    })
+}
+
+resource "aws_lambda_function" "flask_app" {
+  function_name = "flask_hello_cicd"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "app.lambda_handler"
+  runtime       = "python3.11"
+
+  filename         = "${path.module}/../app.zip"
+  source_code_hash = filebase64sha256("${path.module}/../app.zip")
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "flask-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.flask_app.invoke_arn
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_stage.default_stage.invoke_url
+}
