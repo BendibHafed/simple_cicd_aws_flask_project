31a96bf676c55b41a87ffd497fc8cb1b23183e47
BendibHafed <hafededdinebendib@gmail.com>
2025-09-01 02:04:34 +0100

    fix Full URL of the API Gateway-2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "api_gateway_url" {
-  description = "Invoke URL for the API Gateway"
-  value       = aws_apigatewayv2_stage.my_stage.invoke_url
+  description = "Invoke URL for the API Gateway."
+  value       = aws_apigatewayv2_stage.default_stage.invoke_url
 }
 
 output "lambda_function_name" {
